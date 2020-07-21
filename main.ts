b64974ee3d23f4030ee54bd6568c4ed98dc2a5f8
Dgfxk <67552430+Dgfxk@users.noreply.github.com>
2020-07-21 11:28:01 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+    
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+})
+basic.showString("MOI!")
+basic.clearScreen()
